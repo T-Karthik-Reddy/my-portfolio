e41689adb7ed6bd5335604806b484e0fa8da4e60
Theepireddy Karthik Reddy <theepireddykarthikreddy@Theepireddys-MacBook-Pro.local>
2025-06-21 15:59:34 +0530

    Updated Vite config and package dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Replace 'my-portfolio' with your GitHub repository name
+  base: '/my-portfolio/', // <--- ADD THIS LINE
   plugins: [react()],
   server: {
-    port: 3000, // Optional: specify a port
+    port: 3000,
   },
-  // Ensure publicDir is 'public' by default, or specify if different
-  // publicDir: 'public', 
 })
